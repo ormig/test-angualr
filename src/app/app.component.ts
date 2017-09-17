69afbb6b2575dc21b32d7199b43c4e57690a91e5
romanov <ormigus@gmail.com>
2017-09-18 00:21:01 +0300

    First draft version of bills list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  bills: Bill[] = BILLS;
+  selectedBill: Bill;
+
+  select(bill: Bill) {
+    this.selectedBill = bill;
+  }
 }
+
+export class Bill {
+  id: number;
+  name: string;
+  currency: string;
+}
+
+const BILLS: Bill[] = [
+  {id: 0, name: 'Cash', currency: 'RUB'},
+  {id: 1, name: 'Cash', currency: 'EUR'},
+  {id: 2, name: 'Cash', currency: 'USD'},
+  {id: 3, name: 'Sberbank', currency: 'RUB'},
+  {id: 4, name: 'Tinkoff', currency: 'RUB'},
+  {id: 5, name: 'Tinkoff', currency: 'USD'},
+  {id: 6, name: 'Tinkoff', currency: 'EUR'}
+];
